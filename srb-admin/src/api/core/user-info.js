fb5fedada67c1ef36cc9c030185372b99ef2bf85
sy <2637065613@qq.com>
2023-01-09 17:56:37 +0800

    2023/1/9

---
+++ srb-admin/src/api/core/user-info.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+export default {
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `/admin/core/userInfo/list/${page}/${limit}`,
+      method: 'get',
+      params: searchObj,
+    })
+  },
+}
